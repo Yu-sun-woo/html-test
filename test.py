214445b00b2277758f0f6db8ad8c5ea45187cbfc
root <root@DESKTOP-JF0RK43.localdomain>
2020-06-27 15:27:41 +0900

    first test

---
+++ test.py
@@ -0,0 +1,16 @@
+for i in range(10):
+    print(i)
+
+a=0
+
+while a<10:
+    print("a")
+    a+=1
+
+
+def is_odd(a):
+    if a%2==0:
+        print("no")
+    else :
+        print("yes")
+        
